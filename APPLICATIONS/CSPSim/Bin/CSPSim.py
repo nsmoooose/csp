4014ae481bc33d0d1a8f7c80612ccb1b500bde1b
mkrose <mkrose@7152ba15-5bd8-0310-b828-e896570a432b>
2003-07-22 07:42:59 +0000

    *** empty log message ***
    git-svn-id: https://csp.svn.sourceforge.net/svnroot/csp/trunk@624 7152ba15-5bd8-0310-b828-e896570a432b

--- APPLICATIONS/CSPSim/Bin/CSPSim.py
+++ APPLICATIONS/CSPSim/Bin/CSPSim.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python -O
 
+# Combat Simulator Project - CSPSim
+# Copyright (C) 2002 The Combat Simulator Project
+# http://csp.sourceforge.net
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+
 import sys, os, os.path
 
 #import Shell
 #from SimData.Compile import Compiler, CompilerUsageError
 
 # enable lazy loading of shared library modules if available
-if hasattr(sys, "setdlopenflags"):
-	sys.setdlopenflags(0x101)
+if os.name == 'posix':
+	import dl
+	sys.setdlopenflags(dl.RTLD_GLOBAL|dl.RTLD_LAZY)
+
+#if hasattr(sys, "setdlopenflags"):
+#	sys.setdlopenflags(0x101)
 	
 # is the SDL joystick environment variable isn't set, try a standard value
 if not os.environ.has_key("SDL_JOYSTICK_DEVICE"):
--- APPLICATIONS/CSPSim/Bin/TestObjects.py
+++ APPLICATIONS/CSPSim/Bin/TestObjects.py
@@ -1,3 +1,20 @@
+# Combat Simulator Project - CSPSim
+# Copyright (C) 2002 The Combat Simulator Project
+# http://csp.sourceforge.net
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 import SimData
 
@@ -5,7 +22,7 @@ def create(app):
 	v = SimData.Vector3
 	m = SimData.LLA()
 	vehicle = app.createVehicle
-#	vehicle("sim:vehicles.aircraft.m2k", v(10, 10, 1000.2), v(0, 100.0, 0), v(2.0, 2.0, 140.0))
+	vehicle("sim:vehicles.aircraft.f16", v(10, 10, 1000.2), v(0, 100.0, 0), v(2.0, 2.0, 140.0))
 
 # 	vehicle("sim:vehicles.aircraft.m2k", v(483010, 499010, 190.2), v(0, 100.0, 0), v(2.0, 2.0, 140.0))
 # 	vehicle("sim:vehicles.aircraft.m2k", v(483025, 499015, 190.2), v(0, 100.0, 0), v(5.0, 0.0, 240.0))

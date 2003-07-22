bbb27d69af58c1543838c7b921d123534c88c247
mkrose <mkrose@7152ba15-5bd8-0310-b828-e896570a432b>
2003-07-22 07:47:05 +0000

    *** empty log message ***
    git-svn-id: https://csp.svn.sourceforge.net/svnroot/csp/trunk@625 7152ba15-5bd8-0310-b828-e896570a432b

--- APPLICATIONS/SimData/Include/SimData/PTS.h
+++ APPLICATIONS/SimData/Include/SimData/PTS.h
@@ -1,19 +1,6 @@
 /*
  * Simulated Partial Template Specialization
  *
- * Adapted from: metactrl.h
- * by Krzysztof Czarnecki & Ulrich Eisenecker
- */
-
-/* The following functions come from chapter 10 of the indispensable book
- * Generative Programming by Krzysztof Czarnecki & Ulrich Eisenecker
- * (C) Copyright Krzysztof Czarnecki & Ulrich Eisenecker 1998-2000.
- * Permission to copy, use, modify, sell and distribute this software is
- * granted provided this copyright notice appears in all copies. In case of
- * modification, the modified files should carry a notice stating that
- * you changed the files.
- * This software is provided "as is" without express or implied
- * warranty, and with no claim as to its suitability for any purpose.
  */
 
 #if defined(_MSC_VER) && (_MSC_VER <= 1300)

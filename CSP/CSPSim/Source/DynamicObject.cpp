2a1d30a916eab4b054bc7fc2fc417f735e7f51e4
wolverine <wolverine@7152ba15-5bd8-0310-b828-e896570a432b>
2004-08-01 03:09:26 +0000

    formatting changes
    git-svn-id: https://csp.svn.sourceforge.net/svnroot/csp/trunk@1195 7152ba15-5bd8-0310-b828-e896570a432b

--- CSP/CSPSim/Source/DynamicObject.cpp
+++ CSP/CSPSim/Source/DynamicObject.cpp
@@ -1,17 +1,17 @@
 // Combat Simulator Project - FlightSim Demo
 // Copyright (C) 2002 The Combat Simulator Project
 // http://csp.sourceforge.net
-// 
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -117,7 +117,7 @@ void DynamicObject::setGlobalPosition(double x, double y, double z)
 		// and use the local elevation (not implemented)
 		// FIXME FIXME FIXME XXX XXX
 		float offset = 0.0;
-		z = offset; 
+		z = offset;
 	}
 	b_GlobalPosition->value() = simdata::Vector3(x, y, z);
 }
@@ -147,7 +147,7 @@ void DynamicObject::updateGroundPosition()
 	VirtualBattlefield const *battlefield = sim->getBattlefield();
 	assert(battlefield);
 	simdata::Vector3 &pos = b_GlobalPosition->value();
-	pos.z() = battlefield->getGroundElevation(pos.x(), pos.y(), m_GroundHint) + offset; 
+	pos.z() = battlefield->getGroundElevation(pos.x(), pos.y(), m_GroundHint) + offset;
 }
 
 
@@ -203,7 +203,7 @@ void DynamicObject::postUpdate(double dt) {
 		b_GroundZ->value() = battlefield->getGroundElevation(
 			b_GlobalPosition->value().x(), 
 			b_GlobalPosition->value().y(), 
-			b_GroundN->value(), 
+			b_GroundN->value(),
 			m_GroundHint
 		);
 		double height = (b_GlobalPosition->value().z() - b_GroundZ->value()) * b_GroundN->value().z();
@@ -368,7 +368,7 @@ bool DynamicObject::onMapEvent(MapEvent const &event) {
 	if (InputInterface::onMapEvent(event)) {
 		return true;
 	}
-if (m_SystemsModel.valid() && m_SystemsModel->onMapEvent(event)) {
+	if (m_SystemsModel.valid() && m_SystemsModel->onMapEvent(event)) {
 		return true;
 	}
 	return false;

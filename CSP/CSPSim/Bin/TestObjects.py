5bb9bad2552304512409da1d7b11c42883abdb25
delta <delta@7152ba15-5bd8-0310-b828-e896570a432b>
2004-12-15 18:13:51 +0000

    * Moved the aircraft at start up to be at the bottom of the runway.
    git-svn-id: https://csp.svn.sourceforge.net/svnroot/csp/trunk@1389 7152ba15-5bd8-0310-b828-e896570a432b

--- CSP/CSPSim/Bin/TestObjects.py
+++ CSP/CSPSim/Bin/TestObjects.py
@@ -38,7 +38,7 @@ def createBalkan(app):
 	v = SimData.Vector3
 	m = SimData.LLA()
 	vehicle = app.createVehicle
-	vehicle("sim:vehicles.aircraft.m2k", v(-29403, -10980, 88.5), v(0, 0, 0), v(0.0, 0.0, 180.0))
+	vehicle("sim:vehicles.aircraft.m2k", v(-29495, -10530, 88.5), v(0, 0, 0), v(0.0, 0.0, 180.0))
 	#vehicle("sim:vehicles.aircraft.m2k", v(-29413, -10980, 98.75), v(0, 100.0, 0), v(5.0, 0.0, 180.0))
 	#vehicle("sim:vehicles.aircraft.m2k", v(-29413, -10980, 108.75), v(0, 100.0, 0), v(5.0, 0.0, 180.0))
 	#vehicle("sim:vehicles.aircraft.m2k", v(-29413, -10980, 118.75), v(0, 100.0, 0), v(5.0, 0.0, 180.0))

a6bf14e7c4a9d378ab343e63f26455aac17aca86
mkrose <mkrose@7152ba15-5bd8-0310-b828-e896570a432b>
2005-07-15 15:05:46 +0000

    Fix Object unittest.
    git-svn-id: https://csp.svn.sourceforge.net/svnroot/csp/trunk@1597 7152ba15-5bd8-0310-b828-e896570a432b

--- CSP/SimData/SimData/Tests/test_Object.cpp
+++ CSP/SimData/SimData/Tests/test_Object.cpp
@@ -1,5 +1,5 @@
 /* SimData: Data Infrastructure for Simulations
- * Copyright (C) 2003 Mark Rose <tm2@stm.lbl.gov>
+ * Copyright (C) 2003 Mark Rose <mkrose@users.sf.net>
  *
  * This file is part of SimData.
  *
@@ -25,10 +25,12 @@
  */
 
 
-#include <SimData/Object.h>
-#include <SimData/Vector3.h>
-#include <SimData/Link.h>
+#include <SimData/DataArchive.h>
 #include <SimData/InterfaceRegistry.h>
+#include <SimData/Link.h>
+#include <SimData/Object.h>
+#include <SimData/ObjectInterface.h>
+#include <SimData/Vector3.h>
 
 #include <iostream>
 #include <cstdlib>
@@ -38,46 +40,59 @@
 class TestObject: public simdata::Object
 {
 public:
-
-	SIMDATA_OBJECT(TestObject,0,0)
-	BEGIN_SIMDATA_XML_INTERFACE(TestObject)
-		SIMDATA_XML("vector", TestObject::_vector, true)
-		SIMDATA_XML("link", TestObject::_link, true)
-		SIMDATA_XML("bool", TestObject::_bool, false)
-	END_SIMDATA_XML_INTERFACE
+	SIMDATA_DECLARE_OBJECT(TestObject)
 
 	TestObject() {}
 	virtual ~TestObject() {}
 
-private:
-
+public:
 	simdata::Vector3 _vector;
 	simdata::Link<TestObject> _link;
 	bool _bool;
 };
 
+SIMDATA_XML_BEGIN(TestObject)
+	SIMDATA_DEF("vector", _vector, true)
+	SIMDATA_DEF("link", _link, false)
+	SIMDATA_DEF("bool", _bool, false)
+SIMDATA_XML_END
 
-class SubObject: public TestObject
+
+class SubObject1: public TestObject
 {
 public:
+	SIMDATA_DECLARE_STATIC_OBJECT(SubObject1)
 
-	SIMDATA_STATIC_OBJECT(SubObject,0,0)
-	EXTEND_SIMDATA_XML_INTERFACE(SubObject, TestObject)
-		SIMDATA_XML("link1", SubObject::_link1, false)
-		SIMDATA_XML("link2", SubObject::_link2, true)
-	END_SIMDATA_XML_INTERFACE
-
-	SubObject() {}
-	virtual ~SubObject() {}
+	SubObject1() {}
+	virtual ~SubObject1() {}
 
 private:
-
-	simdata::Link<SubObject> _link1;
+	simdata::Link<SubObject1> _link1;
 	simdata::Link<TestObject> _link2;
 };
 
-SIMDATA_REGISTER_INTERFACE(TestObject)
-SIMDATA_REGISTER_INTERFACE(SubObject)
+SIMDATA_XML_BEGIN(SubObject1)
+	SIMDATA_DEF("link1", _link1, false)
+	SIMDATA_DEF("link2", _link2, true)
+SIMDATA_XML_END
+
+
+class SubObject2: public TestObject
+{
+public:
+	SIMDATA_DECLARE_STATIC_OBJECT(SubObject2)
+
+	SubObject2() {}
+	virtual ~SubObject2() {}
+
+public:
+	double _value;
+};
+
+SIMDATA_XML_BEGIN(SubObject2)
+	SIMDATA_DEF("value", _value, false)
+SIMDATA_XML_END
+
 
 void testTestObjectInterface(simdata::InterfaceProxy *i) {
 	assert(i != 0);
@@ -85,7 +100,7 @@ void testTestObjectInterface(simdata::InterfaceProxy *i) {
 	assert(i->variableExists("link"));
 	assert(i->variableExists("bool"));
 	assert(i->variableRequired("vector"));
-	assert(i->variableRequired("link"));
+	assert(!i->variableRequired("link"));
 	assert(!i->variableRequired("bool"));
 	assert(i->variableType("vector") == "type::Vector3");
 	assert(i->variableType("bool") == "builtin::bool");
@@ -101,29 +116,67 @@ void testSubObjectInterface(simdata::InterfaceProxy *i) {
 	assert(i->isSubclass(TestObject::_getClassHash()));
 }
 
+void testSerialize() {
+	// FIXME using tmpfile would be much better but DataArchive doesn't accept open
+	// stream descriptors.
+	const std::string tmpfile("/tmp/simdata.tmptest.dar");
+	{
+		std::cout << "write\n";
+		TestObject obj1;
+		SubObject2 obj2;
+		obj1._vector.y() = 2.17;
+		obj1._link = "obj2";
+		obj2._vector.x() = 42.0;
+		obj2._value = 3.14;
+		simdata::DataArchive ar(tmpfile, /*read=*/false);
+		ar.addObject(obj1, "obj1");
+		ar.addObject(obj2, "obj2");
+		ar.finalize();
+	}
+	{
+		simdata::DataArchive ar(tmpfile, /*read=*/true);
+		simdata::Ref<TestObject> obj = ar.getObject("obj1");
+		assert(obj.valid());
+		assert(std::abs(obj->_vector.y() - 2.17) < 1e-8);
+		assert(obj->_link.valid());
+		simdata::Path p = obj->_link;
+		assert(p == simdata::Path("obj2"));
+		assert(std::abs(obj->_link->_vector.x() - 42.0) < 1e-8);
+		simdata::Ref<SubObject2> sub2 = obj->_link;
+		assert(sub2.valid());
+		assert(std::abs(sub2->_value - 3.14) < 1e-8);
+	}
+}
+
 void test() {
 	// TODO
 	// 	* set and retrieve values
 	// 	* split Object class declarations into a header?
-	simdata::InterfaceRegistry &reg = simdata::InterfaceRegistry::getInterfaceRegistry();
-	assert(reg.hasInterface("TestObject"));
-	assert(reg.hasInterface("SubObject"));
-	assert(reg.hasInterface(TestObject::_getClassHash()));
-	assert(reg.hasInterface(SubObject::_getClassHash()));
-	assert(reg.hasInterface(TestObject::_getClassName()));
-	assert(reg.hasInterface(SubObject::_getClassName()));
-	simdata::InterfaceProxy *i;
-	i = reg.getInterface("TestObject");
-	assert(i->getClassHash() == TestObject::_getClassHash());
-	assert(i->getClassName() == TestObject::_getClassName());
-	assert(!i->isStatic());
-	testTestObjectInterface(i);
-	i = reg.getInterface("SubObject");
-	assert(i->getClassHash() == SubObject::_getClassHash());
-	assert(i->getClassName() == SubObject::_getClassName());
-	assert(i->isStatic());
-	testTestObjectInterface(i);
-	testSubObjectInterface(i);
+	try {
+		simdata::InterfaceRegistry &reg = simdata::InterfaceRegistry::getInterfaceRegistry();
+		assert(reg.hasInterface("TestObject"));
+		assert(reg.hasInterface("SubObject1"));
+		assert(reg.hasInterface(TestObject::_getClassHash()));
+		assert(reg.hasInterface(SubObject1::_getClassHash()));
+		assert(reg.hasInterface(TestObject::_getClassName()));
+		assert(reg.hasInterface(SubObject1::_getClassName()));
+		simdata::InterfaceProxy *i;
+		i = reg.getInterface("TestObject");
+		assert(i->getClassHash() == TestObject::_getClassHash());
+		assert(i->getClassName() == TestObject::_getClassName());
+		assert(!i->isStatic());
+		testTestObjectInterface(i);
+		i = reg.getInterface("SubObject1");
+		assert(i->getClassHash() == SubObject1::_getClassHash());
+		assert(i->getClassName() == SubObject1::_getClassName());
+		assert(i->isStatic());
+		testTestObjectInterface(i);
+		testSubObjectInterface(i);
+		testSerialize();
+	} catch (simdata::Exception e) {
+		e.details();
+		::exit(1);
+	}
 	std::cout << "OK\n";
 	::exit(0);
 }

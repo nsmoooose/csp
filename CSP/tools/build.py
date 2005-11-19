b0197375e0546e1b5a621638d73a3e789d7b949d
mkrose <mkrose@7152ba15-5bd8-0310-b828-e896570a432b>
2005-11-19 19:29:23 +0000

    Update build.py to work with SCons 0.96.91 (and 0.96.1).
    git-svn-id: https://csp.svn.sourceforge.net/svnroot/csp/trunk@1677 7152ba15-5bd8-0310-b828-e896570a432b

--- CSP/tools/build.py
+++ CSP/tools/build.py
@@ -36,12 +36,15 @@ import SCons
 import SCons.Builder
 from SCons.Builder import Builder
 from SCons.Script import SConscript
+from SCons.Script.SConscript import SConsEnvironment
 from SCons.Action import Action
 from SCons.Scanner import Scanner
 from SCons.Node.FS import default_fs
 
 File = default_fs.File
 Dir = default_fs.Dir
+
+SConsVersion = tuple(map(int, SCons.__version__.split('.')))
 
 
 PYTHON_INCLUDE = sysconfig.get_python_inc()
@@ -75,28 +78,43 @@ def IsWindows(env):
 	return env['PLATFORM'].startswith('win')
 
 
+def SimpleCommand(cmd, msg):
+	def s(target, src, env):
+		print msg % {'src':str(src), 'target':str(target[0])}
+	return SCons.Action.CommandAction(cmd, strfunction=s)
+
+
 ############################################################################
-# SHORT MESSAGES
+# BUILD LOG AND DISPLAY MESSAGES
+#
+# The code here modifies the way scons displays build operations.  As of
+# version 0.96.91, SCons provides a relatively simple mechanism for
+# customizing build messages via environment variables.  Unfortunately
+# this feature has some bugs and significant limitations.  In particular,
+# it isn't possible to customize the output for certian types of commands
+# (such as $ARCOM, which is a known issue that will probably be fixed in
+# the next release), and there is no easy way to simplify the display output
+# while still logging the full build commands.
+#
+# Avoiding these limitations requires low-level tweaks to the scons Action
+# classes.  This is extremely brittle and prone to breakage with each new
+# SCons release, although hopefully soon SCons will provide good enough
+# support for build logging and message customization that these hacks will
+# no longer be needed.
 
-class ShortMessageWrapper:
-	def __init__(self, message):
-		self.message = message
-		self.old_message = None
 
-	def bind(self, action):
-		if not isinstance(action.strfunction, ShortMessageWrapper):
-			self.old_message = action.strfunction
-			action.strfunction = self
-
-	def __call__(self, target, source, env):
-		if self.old_message and BUILD_LOG:
-			old = self.old_message(target, source, env)
-			print >>BUILD_LOG, old
-			BUILD_LOG.flush()
-		return self.message(target, source, env)
+def WriteToBuildLog(message):
+	"""
+	Write a message to the build log.  The log should contain a verbose
+	record of the actual commands that were executed.
+	"""
+	if message and BUILD_LOG:
+		print >>BUILD_LOG, message
+		BUILD_LOG.flush()
 
 
 def TargetMessage(phrase, suffix=''):
+	"""Simplified build message function that displays the first target."""
 	def printer(target, source, env, phrase=phrase, suffix=suffix):
 		if type(target) == type([]):
 			target = map(str, target)
@@ -106,6 +124,7 @@ def TargetMessage(phrase, suffix=''):
 
 
 def SourceMessage(phrase, suffix='', limit=None):
+	"""Simplified build message function that displays the first source."""
 	def printer(target, source, env, phrase=phrase, suffix=suffix):
 		if type(source) == type([]):
 			source = map(str, source)
@@ -117,11 +136,115 @@ def SourceMessage(phrase, suffix='', limit=None):
 
 
 def SourceToTargetMessage(phrase, suffix=''):
+	"""Simplified build message function that displays the first source and target."""
 	def printer(target, source, env, phrase=phrase, suffix=suffix):
 		source = map(str, source)
 		target = map(str, target)
 		print '- %s %s to %s %s' % (phrase, source[0], target[0], suffix)
 	return printer
+
+
+def ActionCallWrapper(self, target, source, env, *args, **kw):
+	"""
+	A replacement for SCons.Action._ActionAction.__call___.  This function wraps
+	the original __call__ implementation to provide low-level hooks for logging
+	and displaying build messsages.
+	
+	Note that this is an extremely brittle hack than should be removed If and
+	when SCons provides better native support for these customizations.
+
+	Rationale: As of SCons 0.96.91, the introduction of memoization and lazy
+	action evaluation makes it impossible to directly modify many of the
+	important Action instances (since they are generated on the fly during the
+	build).  The only customization hook is to override strfunction, but the
+	replacement function does not have access to the Action instance and so
+	cannot generate the actual build command string.
+	"""
+	if not SCons.Util.is_List(target):
+		target = [target]
+	if not SCons.Util.is_List(source):
+		source = [source]
+
+	# generate the full build message by directly calling the original strfunction
+	# implementation.
+	log = self.__class__.strfunction(self, target, source, env)
+	WriteToBuildLog(log)
+
+	# if the strfunction has been replaced with our ShortMessageWrapper helper we
+	# pass the action and verbose log message as additional arguments.  these are
+	# currently needed in order to suppress duplicate display messages for some
+	# multi-action build steps (such as $ARCOM).
+	if isinstance(self.strfunction, ShortMessageWrapper):
+		self.strfunction(target, source, env, action=self, log=log)
+	else:
+		if self.strfunction is not None:
+			self.strfunction(target, source, env)
+
+	# disable build message display in the original __call__ implementation.
+	# ideally we would let the original method handle build logging via
+	# env['PRINT_CMD_LINE_FUNC'] and this wrapper would just provide the
+	# short message display, but with the new memoization infrastructure in
+	# SCons the only way to get a custom build function into the action is
+	# to override strfunction.  (we could swap strfunction here, but that would
+	# cause problems for parallel builds.)
+	if len(args) > 2:
+		args = list(args)
+		args[2] = 0
+	else:
+		kw['show'] = 0
+
+	# now call the original __call__ implementation.
+	return self._internal_call(target, source, env, *args, **kw)
+
+
+class ShortMessageWrapper:
+	def __init__(self, message):
+		self.message = message
+
+	def bind(self, env, action):
+		if SConsVersion >= (0, 96, 91):
+			self._bindNew(env, action)
+		else:
+			self._bindOld(env, action)
+
+	def _bindNew(self, env, action):
+		if isinstance(action, SCons.Action.LazyAction):
+			action.gen_kw['strfunction'] = self
+			#self._bind(env, action)
+			#self.action = action
+			return
+		if isinstance(action, SCons.Action.CommandGeneratorAction) and hasattr(action, 'generator'):
+			if isinstance(action.generator, SCons.Builder.DictCmdGenerator):
+				for ext, subaction in action.generator.items():
+					if subaction:
+						self._bind(env, subaction)
+					else:
+						print 'warning: unable to attach short message wrapper to action (%s) for extension "%s"' % (self.message, ext)
+				return
+		if isinstance(action, SCons.Action.ListAction):
+			for act in action.list:
+				self._bind(env, act)
+			return
+		self._bind(env, action)
+
+	def _bindOld(self, env, action):
+		if isinstance(action, SCons.Action.ListAction):
+			for act in action.list:
+				self._bind(env, act)
+			return
+		self._bind(env, action)
+
+	def _bind(self, env, action):
+		strfunction = getattr(action, 'strfunction', None)
+		if not isinstance(strfunction, ShortMessageWrapper):
+			action.strfunction = self
+
+	def __call__(self, target, source, env, action=None, log=''):
+		# nasty hack.  staticlib and sharedlib are multistep actions, but we only
+		# want to display one message.  here we suppress output for the less interesting
+		# step(s).
+		if not log.startswith('ranlib') and not log.startswith('SharedFlagChecker'):
+			return self.message(target, source, env)
 
 
 def SetShortMessage(env, builder, message):
@@ -141,14 +264,20 @@ def SetShortMessage(env, builder, message):
 			if type(action) == type(''):
 				action = Action(action)
 				builders[builder].add_action(extension, action)
-		if isinstance(action, SCons.Action.ListAction):
-			for a in action.get_actions():
-				if a.strfunction is not None:
-					ShortMessageWrapper(message).bind(a)
-					return
-		ShortMessageWrapper(message).bind(action)
+		ShortMessageWrapper(message).bind(env, action)
+
+
+def InstallMessageHook():
+	try:
+		action_class = SCons.Action._ActionAction
+	except:
+		action_class = SCons.Action.Action  # version 0.96.1 and earlier
+	action_class._internal_call = action_class.__call__
+	action_class.__call__ = ActionCallWrapper
+
 
 def SetShortMessages(env):
+	InstallMessageHook()
 	SetShortMessage(env, 'Program', TargetMessage('Building'))
 	SetShortMessage(env, 'StaticObject', SourceMessage('Compiling'))
 	SetShortMessage(env, 'SharedObject', SourceMessage('Compiling', suffix=' (shared)'))
@@ -382,20 +511,25 @@ def SetDefaultMessage(env, message):
 
 
 def GlobalSetup(env, distributed=1, short_messages=None, default_message=None, with_swig=1):
-	import SCons.Script
+	try:
+		options = SCons.Script.Main.options
+		ssoptions = SCons.Script.Main.ssoptions
+	except:
+		options = SCons.Script.options  # version 0.96.1
+		ssoptions = SCons.Script.ssoptions
+
 	if default_message is not None:
 		SetDefaultMessage(env, default_message)
 	if with_swig:
 		AddSwigSupport(env)
 	if short_messages is None:
-		short_messages = SCons.Script.options.no_progress
+		short_messages = options.no_progress
 	AddBuilders(env)
 	if short_messages:
 		SetShortMessages(env)
-	if distributed and SCons.Script.ssoptions.get('num_jobs') > 1:
+	if distributed and ssoptions.get('num_jobs') > 1:
 		SetDistributed(env)
 	AddPhonyTarget(env, 'config')
-	from SCons.Script.SConscript import SConsEnvironment
 	SConsEnvironment.SetConfig = SetConfig
 	SConsEnvironment.Documentation = MakeDocumentation
 
@@ -492,7 +626,7 @@ def AddCopyFile(env):
 		source = str(source[0])
 		target = str(target[0])
 		return 'copying %s -> %s' % (source, target)
-	CopyFile = Builder(action=Action(copy, report))
+	CopyFile = Builder(action=Action(copy, strfunction=report))
 	env.Append(BUILDERS = {'CopyFile': CopyFile})
 
 
@@ -541,8 +675,9 @@ def AddBuilders(env):
 
 def MarkVersionSource(env, target, prefix='Version.'):
 	if type(target) == type([]): target = target[0]
-	version = [child for child in target.children() if str(child).startswith(prefix)]
-	others = [child for child in target.children() if not str(child).startswith(prefix)]
+	if hasattr(target, 'children'): target = target.children()
+	version = [child for child in target if str(child).startswith(prefix)]
+	others = [child for child in target if not str(child).startswith(prefix)]
 	if version: env.Depends(version, others)
 
 

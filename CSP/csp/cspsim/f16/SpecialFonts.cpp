3b2c408074cb4eebb4235244550168c1bf87085a
mkrose <mkrose@7152ba15-5bd8-0310-b828-e896570a432b>
2006-01-22 03:47:58 +0000

    Move f16 specialized font implementations out of the header and add a custom font cache that is keyed on both facade type and filename. This allows multiple custom facades to be used safely with the same font file and avoids segfaults that were possible with the old implementation.
    git-svn-id: https://csp.svn.sourceforge.net/svnroot/csp/trunk@1829 7152ba15-5bd8-0310-b828-e896570a432b

---
+++ CSP/csp/cspsim/f16/SpecialFonts.cpp
@@ -0,0 +1,288 @@
+// Combat Simulator Project
+// Copyright (C) 2005 The Combat Simulator Project
+// http://csp.sourceforge.net
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+
+/**
+ * @file SpecialFonts.cpp
+ *
+ **/
+
+#include <csp/cspsim/f16/SpecialFonts.h>
+#include <csp/cspsim/Config.h>
+#include <csp/csplib/util/FileUtility.h>
+#include <csp/csplib/util/Log.h>
+#include <csp/csplib/util/StringTools.h>
+#include <csp/csplib/thread/Synchronization.h>
+
+#include <osg/Version>
+#include <osgDB/ReaderWriter>
+#include <osgDB/ReadFile>
+
+#include <algorithm>
+#include <cassert>
+#include <map>
+
+// first defined in osg 0.9.9
+#ifndef OSG_VERSION_MAJOR
+#define OSG_OLD_FONT_INTERFACE
+#endif
+
+CSP_NAMESPACE
+
+
+namespace {
+
+// osgText::readFontFile caches results by filename, but sharing a single FontImplementation
+// between multiple special font classes does not work (FontImplementation supports only a
+// single facade).  Instead we manage our own cache that is keyed on both filename and special
+// font type.  Note that font path lookup currently uses the config font directory (as set in
+// the .ini file) rather than the full osg search path.
+class SpecialFontCache {
+public:
+	static void clear() {
+		MutexLock lock(m_Mutex);
+		m_Cache.clear();
+	}
+
+	static osgText::Font* load(std::string const &filename, std::string const &cache_prefix, osgText::Font *prototype) {
+		MutexLock lock(m_Mutex);
+		std::string key(cache_prefix + filename);
+		osgText::Font *font = get(key);
+		if (font) return font;
+		std::string fontdir = getDataPath("FontPath");
+		std::string fontpath = ospath::join(fontdir, filename);
+		if (!ospath::exists(fontpath)) {
+			CSPLOG(ERROR, DATA) << "Unable to find font " << fontpath;
+			return 0;
+		}
+		// force the file to be reloaded; the osg cached version (if any) is used for
+		// regular osgText::Font instances.
+		osg::ref_ptr<osgDB::ReaderWriter::Options> options = new osgDB::ReaderWriter::Options;
+		options->setObjectCacheHint(osgDB::ReaderWriter::Options::CACHE_NONE);
+		osg::Object* object = osgDB::readObjectFile(fontpath, options.get());
+		font = dynamic_cast<osgText::Font*>(object);
+		if (!font) {
+			CSPLOG(ERROR, DATA) << "Invalid font " << fontpath;
+			// if the object has zero references then delete it by doing another unref().
+			if (object && object->referenceCount()==0) object->unref();
+			return 0;
+		}
+
+		// grab a reference to the implementation and delete the original font instance.
+		// we must delete the original instance before assigning the implementation to
+		// the prototype, since the Font destructor clears implementation->facade_.
+		// this is a hack, much like the entire caching scheme implemented here.  it
+		// would be nice if osg provided a better mechanism for creating custom font
+		// overrides.  of course an alternative approach would be to simply specialize
+		// the fonts themselves rather than the rendering code, but that is not very
+		// flexible.
+		osg::ref_ptr<osgText::Font::FontImplementation> implementation(font->getImplementation());
+		{ osg::ref_ptr<osgText::Font> cleaner(font); } // ref and unref to delete.
+
+		assert(prototype);
+		CSPLOG(INFO, DATA) << "Assigning font implementation " << font->getImplementation() << " to " << prototype;
+		prototype->setImplementation(implementation.get());
+		set(key, prototype);
+		return prototype;
+	}
+
+private:
+	typedef std::map<std::string, osg::ref_ptr<osgText::Font> > CacheMap;
+	static CacheMap m_Cache;
+	static Mutex m_Mutex;
+
+	static osgText::Font *get(std::string const &key) {
+		CacheMap::iterator iter = m_Cache.find(key);
+		return (iter == m_Cache.end()) ? 0 : iter->second.get();
+	}
+
+	static void set(std::string const &key, osgText::Font *font) {
+		m_Cache.insert(std::make_pair(key, osg::ref_ptr<osgText::Font>(font)));
+	}
+};
+
+SpecialFontCache::CacheMap SpecialFontCache::m_Cache;
+Mutex SpecialFontCache::m_Mutex;
+
+} // namespace
+
+osgText::Font* ReverseAltFont::load(std::string const &filename) {
+	osg::ref_ptr<osgText::Font> prototype = new ReverseAltFont;
+	return SpecialFontCache::load(filename, "REV:", prototype.get());
+}
+
+osgText::Font* ScaledAltFont::load(std::string const &filename, float scale, bool center_vertical) {
+	std::string prefix = stringprintf("SCA(%e,%d):", scale, center_vertical ? 1 : 0);
+	osg::ref_ptr<osgText::Font> prototype = new ScaledAltFont(scale, center_vertical);
+	return SpecialFontCache::load(filename, prefix, prototype.get());
+}
+
+ReverseAltFont::ReverseAltFont() {
+	CSPLOG(INFO, DATA) << "ReverseAltFont " << this;
+}
+
+ReverseAltFont::~ReverseAltFont() {
+	CSPLOG(INFO, DATA) << "~ReverseAltFont " << this << ", imp=" << getImplementation();
+}
+
+osgText::Font::Glyph* ReverseAltFont::getGlyph(unsigned int charcode) {
+	SizeGlyphMap::iterator itr = _sizeGlyphMap.find(SizePair(_width,_height));
+	if (itr != _sizeGlyphMap.end()) {
+		GlyphMap& glyphmap = itr->second;
+		GlyphMap::iterator gitr = glyphmap.find(charcode);
+		if (gitr!=glyphmap.end()) return gitr->second.get();
+	}
+	if (_implementation.valid()) {
+		if (charcode >= 128) {
+			// use '0' as the reference glyph for sizing and centering the new glyph.  'M' would
+			// probably be a better choice, but i haven't made that glyph yet in my test font ;-)
+			Glyph *reference = getGlyph('0');
+			Glyph *normal = getGlyph(charcode % 128);
+			Glyph *reverse = new osgText::Font::Glyph;
+			unsigned int reference_width = reference->s();
+			unsigned int reference_height = reference->t();
+			unsigned int source_width = normal->s();
+			unsigned int source_height = normal->t();
+
+			// the new glyph should fit the full character cell.  we expand the horizontal dimensions slightly
+			// to compensate for texture blending at the edges of the glyph that might otherwise cause a gap
+			// between adjacent reverse video characters.
+			unsigned int cell_height = static_cast<unsigned int>(reference->getVerticalAdvance() + 0.5);
+			unsigned int cell_width = static_cast<unsigned int>(reference->getHorizontalAdvance() + 0.5) + 2 * getGlyphImageMargin();
+
+			// sanity checking
+			cell_width = std::max(cell_width, source_width);
+			cell_height = std::max(cell_height, source_height);
+			unsigned int cell_data_size = cell_height * cell_width;
+			unsigned int data_size = normal->getImageSizeInBytes();
+			unsigned int bits_per_pixel = normal->getPixelSizeInBits();
+			assert(bits_per_pixel == 8);
+			assert(normal->r() == 1);
+			assert(cell_height >= source_height);
+			assert(cell_width >= source_width);
+			assert(data_size = source_width * source_height);
+
+			// create and initialize the image array for the new glyph
+			unsigned char const *image = normal->data();
+			unsigned char *data = new unsigned char[cell_data_size];
+			for (unsigned char *init = data; init < data + cell_data_size; ) {
+				*init++ = 0xff;  // opaque
+			}
+
+			// the new glyph cell is position based on the reference character.  this ensures that
+			// the edges line up correctly for different characters.  the actual glyph needs to be
+			// offset relative to the reference character based on the character metrics.  this
+			// offset is hard-coded into the new image, since the metrics for the new glyph will
+			// be based entirely on the reference glyph.
+			osg::Vec2 glyph_offset = reference->getHorizontalBearing() - normal->getHorizontalBearing();
+			unsigned int offset_top = static_cast<unsigned int>(std::max(0.0f, (cell_height - reference_height) / 2 - glyph_offset.y()));
+			unsigned int offset_left = static_cast<unsigned int>(std::max(0.0f, (cell_width - reference_width) / 2 - glyph_offset.x()));
+			unsigned char const *source = image;
+			unsigned char *copy = data + offset_top * cell_width + offset_left;
+			for (unsigned int j = 0; j < source_height; j++) {
+				for (unsigned int i = 0; i < source_width; i++) {
+					*copy++ = *source++ ^ 0xff;
+				}
+				copy += cell_width - source_width;
+			}
+			reverse->setImage(cell_width, cell_height, 1, GL_ALPHA, GL_ALPHA, GL_UNSIGNED_BYTE, data, osg::Image::USE_NEW_DELETE, 1);
+
+			// finally set the metrics such that the reference glyph would be centered in the
+			// cell.  offsets for this specific glyph have already been accounted for when
+			// generating the new glyph image.
+			osg::Vec2 horizontal_bearing = reference->getHorizontalBearing();
+			horizontal_bearing.x() -= (cell_width - reference_width) / 2;
+			horizontal_bearing.y() -= (cell_height - reference_height) / 2;
+			reverse->setHorizontalBearing(horizontal_bearing);
+
+			// force uniform spacing, even if the font has variable width characters
+			reverse->setHorizontalAdvance(reference->getHorizontalAdvance());
+
+			// no adjustments to the vertical metrics for now; ReverseAltFont is intended for
+			// horizontal text only.
+			reverse->setVerticalBearing(reference->getVerticalBearing());
+			reverse->setVerticalAdvance(reference->getVerticalAdvance());
+
+			// finally add the new glyph so we don't repeat all this work!
+#ifdef OSG_OLD_FONT_INTERFACE
+			addGlyph(getWidth(), getHeight(), charcode, reverse);
+#else
+			addGlyph(getFontWidth(), getFontHeight(), charcode, reverse);
+#endif
+			return reverse;
+		} else {
+			return _implementation->getGlyph(charcode);
+		}
+	}
+	return 0;
+}
+
+osgText::Font::Glyph* ScaledAltFont::getGlyph(unsigned int charcode) {
+	SizeGlyphMap::iterator itr = _sizeGlyphMap.find(SizePair(_width,_height));
+	if (itr != _sizeGlyphMap.end()) {
+		GlyphMap& glyphmap = itr->second;
+		GlyphMap::iterator gitr = glyphmap.find(charcode);
+		if (gitr!=glyphmap.end()) return gitr->second.get();
+	}
+	if (_implementation.valid()) {
+		if (charcode >= 128) {
+			unsigned int normal_code = charcode % 128;
+			// Ensure that the normal character is loaded.
+			Glyph *normal_glyph = getGlyph(normal_code);
+			// Reload a scaled version of the glyph
+			unsigned int original_width = _width;
+			unsigned int original_height = _height;
+			unsigned int new_width = static_cast<unsigned int>(_width * m_Scale);
+			unsigned int new_height = static_cast<unsigned int>(_height * m_Scale);
+#ifdef OSG_OLD_FONT_INTERFACE
+			setSize(new_width, new_height);
+#else
+			setFontResolution(new_width, new_height);
+#endif
+			Glyph *scaled_glyph = _implementation->getGlyph(normal_code);
+			if (m_VCenter) {
+				osg::Vec2 hbearing = scaled_glyph->getHorizontalBearing();
+				hbearing.y() += 0.5f * (normal_glyph->t() - scaled_glyph->t());
+				scaled_glyph->setHorizontalBearing(hbearing);
+			}
+#ifdef OSG_OLD_FONT_INTERFACE
+			setSize(original_width, original_height);
+#else
+			setFontResolution(original_width, original_height);
+#endif
+			_sizeGlyphMap[SizePair(_width, _height)][charcode] = scaled_glyph;
+			return scaled_glyph;
+		} else {
+			return _implementation->getGlyph(charcode);
+		}
+	}
+	return 0;
+}
+
+CSP_NAMESPACE_END
+
+
+/*
+	ScaledAltFont(std::string const &file, float scale, bool center_vertical=true): m_Scale(scale), m_VCenter(center_vertical) {
+		Font *font = osgText::readFontFile(file);
+		assert(font);
+		setImplementation(font->getImplementation());
+	}
+
+	ScaledAltFont(Font *font, float scale, bool center_vertical=true): Font(font->getImplementation()), m_Scale(scale), m_VCenter(center_vertical) { }
+*/
--- CSP/csp/cspsim/f16/SpecialFonts.h
+++ CSP/csp/cspsim/f16/SpecialFonts.h
@@ -33,15 +33,8 @@
 
 #include <csp/csplib/util/Namespace.h>
 
-#include <osg/Version>
 #include <osgText/Font>
 #include <string>
-#include <algorithm>
-
-// first defined in osg 0.9.9
-#ifndef OSG_VERSION_MAJOR
-#define OSG_OLD_FONT_INTERFACE
-#endif
 
 CSP_NAMESPACE
 
@@ -51,27 +44,12 @@ CSP_NAMESPACE
  */
 class ReverseAltFont: public osgText::Font {
 public:
-
 	/** Construct a reverse font from the specified font file.  The first 128
 	 *  glyphs are rendered normally, while the second 128 glyphs are rendered
 	 *  as reverse-video copies of the first 128 glyphs.  The original glyphs
 	 *  above character 127 are not available.
-	 *
-	 *  WARNING: It is untested and probably unwise to construct both a regular
-	 *  font and a reverse font from the same font file.
 	 */
-	ReverseAltFont(std::string const &file) {
-		Font *font = osgText::readFontFile(file);
-		assert(font);
-		setImplementation(font->getImplementation());
-	}
-
-	/** Construct a reverse font from an existing Font.  The source font should
-	 *  not be accessed either before or after constructing the ReverseAltFont.
-	 *  See the other constructor comment for more information.
-	 */
-	ReverseAltFont(Font *font): Font(font->getImplementation()) {
-	}
+	static osgText::Font* load(std::string const &filename);
 
 	/** Override the default getGlyph method to generate reverse video
 	 *  glyphs for character codes 128 through 255.  For character codes
@@ -81,97 +59,12 @@ public:
 	 *  value inverted.  The image is also expanded to fill the entire
 	 *  character cell.
 	 */
-	virtual Glyph* getGlyph(unsigned int charcode) {
-		SizeGlyphMap::iterator itr = _sizeGlyphMap.find(SizePair(_width,_height));
-		if (itr != _sizeGlyphMap.end()) {
-			GlyphMap& glyphmap = itr->second;
-			GlyphMap::iterator gitr = glyphmap.find(charcode);
-			if (gitr!=glyphmap.end()) return gitr->second.get();
-		}
-		if (_implementation.valid()) {
-			if (charcode >= 128) {
-				// use '0' as the reference glyph for sizing and centering the new glyph.  'M' would
-				// probably be a better choice, but i haven't made that glyph yet in my test font ;-)
-				Glyph *reference = getGlyph('0');
-				Glyph *normal = getGlyph(charcode % 128);
-				Glyph *reverse = new osgText::Font::Glyph;
-				unsigned int reference_width = reference->s();
-				unsigned int reference_height = reference->t();
-				unsigned int source_width = normal->s();
-				unsigned int source_height = normal->t();
+	virtual Glyph* getGlyph(unsigned int charcode);
 
-				// the new glyph should fit the full character cell.  we expand the horizontal dimensions slightly
-				// to compensate for texture blending at the edges of the glyph that might otherwise cause a gap
-				// between adjacent reverse video characters.
-				unsigned int cell_height = static_cast<unsigned int>(reference->getVerticalAdvance() + 0.5);
-				unsigned int cell_width = static_cast<unsigned int>(reference->getHorizontalAdvance() + 0.5) + 2 * getGlyphImageMargin();
-
-				// sanity checking
-				cell_width = std::max(cell_width, source_width);
-				cell_height = std::max(cell_height, source_height);
-				unsigned int cell_data_size = cell_height * cell_width;
-				unsigned int data_size = normal->getImageSizeInBytes();
-				unsigned int bits_per_pixel = normal->getPixelSizeInBits();
-				assert(bits_per_pixel == 8);
-				assert(normal->r() == 1);
-				assert(cell_height >= source_height);
-				assert(cell_width >= source_width);
-				assert(data_size = source_width * source_height);
-
-				// create and initialize the image array for the new glyph
-				unsigned char const *image = normal->data();
-				unsigned char *data = new unsigned char[cell_data_size];
-				for (unsigned char *init = data; init < data + cell_data_size; ) {
-					*init++ = 0xff;  // opaque
-				}
-
-				// the new glyph cell is position based on the reference character.  this ensures that
-				// the edges line up correctly for different characters.  the actual glyph needs to be
-				// offset relative to the reference character based on the character metrics.  this
-				// offset is hard-coded into the new image, since the metrics for the new glyph will
-				// be based entirely on the reference glyph.
-				osg::Vec2 glyph_offset = reference->getHorizontalBearing() - normal->getHorizontalBearing();
-				unsigned int offset_top = static_cast<unsigned int>(std::max(0.0f, (cell_height - reference_height) / 2 - glyph_offset.y()));
-				unsigned int offset_left = static_cast<unsigned int>(std::max(0.0f, (cell_width - reference_width) / 2 - glyph_offset.x()));
-				unsigned char const *source = image;
-				unsigned char *copy = data + offset_top * cell_width + offset_left;
-				for (unsigned int j = 0; j < source_height; j++) {
-					for (unsigned int i = 0; i < source_width; i++) {
-						*copy++ = *source++ ^ 0xff;
-					}
-					copy += cell_width - source_width;
-				}
-				reverse->setImage(cell_width, cell_height, 1, GL_ALPHA, GL_ALPHA, GL_UNSIGNED_BYTE, data, osg::Image::USE_NEW_DELETE, 1);
-
-				// finally set the metrics such that the reference glyph would be centered in the
-				// cell.  offsets for this specific glyph have already been accounted for when
-				// generating the new glyph image.
-				osg::Vec2 horizontal_bearing = reference->getHorizontalBearing();
-				horizontal_bearing.x() -= (cell_width - reference_width) / 2;
-				horizontal_bearing.y() -= (cell_height - reference_height) / 2;
-				reverse->setHorizontalBearing(horizontal_bearing);
-
-				// force uniform spacing, even if the font has variable width characters
-				reverse->setHorizontalAdvance(reference->getHorizontalAdvance());
-
-				// no adjustments to the vertical metrics for now; ReverseAltFont is intended for
-				// horizontal text only.
-				reverse->setVerticalBearing(reference->getVerticalBearing());
-				reverse->setVerticalAdvance(reference->getVerticalAdvance());
-
-				// finally add the new glyph so we don't repeat all this work!
-#ifdef OSG_OLD_FONT_INTERFACE
-				addGlyph(getWidth(), getHeight(), charcode, reverse);
-#else
-				addGlyph(getFontWidth(), getFontHeight(), charcode, reverse);
-#endif
-				return reverse;
-			} else {
-				return _implementation->getGlyph(charcode);
-			}
-		}
-		return 0;
-	}
+	~ReverseAltFont();
+private:
+	// Created only via load()
+	ReverseAltFont();
 };
 
 
@@ -180,63 +73,21 @@ public:
  *  character codes 0-127.
  */
 class ScaledAltFont: public osgText::Font {
-	float m_Scale;
-	bool m_VCenter;
-
 public:
-	ScaledAltFont(std::string const &file, float scale, bool center_vertical=true): m_Scale(scale), m_VCenter(center_vertical) {
-		Font *font = osgText::readFontFile(file);
-		assert(font);
-		setImplementation(font->getImplementation());
-	}
-
-	ScaledAltFont(Font *font, float scale, bool center_vertical=true): Font(font->getImplementation()), m_Scale(scale), m_VCenter(center_vertical) { }
+	static osgText::Font* load(std::string const &filename, float scale, bool center_vertical=true);
 
 	/** Override the default getGlyph method to generate expanded
 	 *  glyphs for character codes 128 through 255.  For character codes
 	 *  less than 128 we use the base class implementation.
 	 */
-	virtual Glyph* getGlyph(unsigned int charcode) {
-		SizeGlyphMap::iterator itr = _sizeGlyphMap.find(SizePair(_width,_height));
-		if (itr != _sizeGlyphMap.end()) {
-			GlyphMap& glyphmap = itr->second;
-			GlyphMap::iterator gitr = glyphmap.find(charcode);
-			if (gitr!=glyphmap.end()) return gitr->second.get();
-		}
-		if (_implementation.valid()) {
-			if (charcode >= 128) {
-				unsigned int normal_code = charcode % 128;
-				// Ensure that the normal character is loaded.
-				Glyph *normal_glyph = getGlyph(normal_code);
-				// Reload a scaled version of the glyph
-				unsigned int original_width = _width;
-				unsigned int original_height = _height;
-				unsigned int new_width = static_cast<unsigned int>(_width * m_Scale);
-				unsigned int new_height = static_cast<unsigned int>(_height * m_Scale);
-#ifdef OSG_OLD_FONT_INTERFACE
-				setSize(new_width, new_height);
-#else
-				setFontResolution(new_width, new_height);
-#endif
-				Glyph *scaled_glyph = _implementation->getGlyph(normal_code);
-				if (m_VCenter) {
-					osg::Vec2 hbearing = scaled_glyph->getHorizontalBearing();
-					hbearing.y() += 0.5f * (normal_glyph->t() - scaled_glyph->t());
-					scaled_glyph->setHorizontalBearing(hbearing);
-				}
-#ifdef OSG_OLD_FONT_INTERFACE
-				setSize(original_width, original_height);
-#else
-				setFontResolution(original_width, original_height);
-#endif
-				_sizeGlyphMap[SizePair(_width, _height)][charcode] = scaled_glyph;
-				return scaled_glyph;
-			} else {
-				return _implementation->getGlyph(charcode);
-			}
-		}
-		return 0;
-	}
+	virtual Glyph* getGlyph(unsigned int charcode);
+
+private:
+	// Created only via load()
+	ScaledAltFont(float scale, bool center_vertical): m_Scale(scale), m_VCenter(center_vertical) { }
+
+	float m_Scale;
+	bool m_VCenter;
 };
 
 CSP_NAMESPACE_END

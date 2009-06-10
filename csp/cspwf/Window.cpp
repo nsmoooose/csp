26c6cd223bed518909a86ce769c037cdf78d3364
nsmoooose <nsmoooose@7152ba15-5bd8-0310-b828-e896570a432b>
2009-06-10 20:25:36 +0000

    Adding a function on the csp::wf::Window class that creates some default style objects.
    This defaults are only used from example code. It will provide some very basic things like background color, borders etc.
    It makes all the csp::wf::examples a lot shorter.
    
    git-svn-id: https://csp.svn.sourceforge.net/svnroot/csp/trunk@2388 7152ba15-5bd8-0310-b828-e896570a432b

--- csp/cspwf/Window.cpp
+++ csp/cspwf/Window.cpp
@@ -32,8 +32,8 @@ namespace csp {
 
 namespace wf {
 
-Window::Window() : 
-	SingleControlContainer("Window"), 
+Window::Window() :
+	SingleControlContainer("Window"),
 	m_StringResources(new StringResourceManager) {
 }
 
@@ -90,7 +90,7 @@ const Window* Window::getWindow(const Control* control) {
 	const Window* window = dynamic_cast<const Window*>(control);
 	if(window != NULL) {
 		return window;
-	}	
+	}
 	return getWindow(control->getParent());
 }
 
@@ -107,7 +107,7 @@ StringResourceManager* Window::getStringResourceManager() {
 }
 
 const StringResourceManager* Window::getStringResourceManager() const {
-	return m_StringResources.get();	
+	return m_StringResources.get();
 }
 
 optional<Ref<Style> > Window::getNamedStyle(const std::string& name) const {
@@ -128,17 +128,17 @@ void Window::centerWindow() {
 	if(!m_WindowManager.valid()) {
 		return;
 	}
-	
+
 	Size screenSize = m_WindowManager->getScreenSize();
 	Size windowSize = getSize();
-		
-	Point windowLocation = 
-		Point(screenSize.width / 2 - windowSize.width / 2, 
+
+	Point windowLocation =
+		Point(screenSize.width / 2 - windowSize.width / 2,
 		screenSize.height / 2 - windowSize.height / 2);
 
 	Ref<Style> style = getStyle();
 	style->setLeft(Style::UnitValue(Style::Pixels, windowLocation.x));
-	style->setTop(Style::UnitValue(Style::Pixels, windowLocation.y));		
+	style->setTop(Style::UnitValue(Style::Pixels, windowLocation.y));
 
 	layoutChildControls();
 	performLayout();
@@ -160,6 +160,96 @@ void Window::maximizeWindow() {
 	performLayout();
 }
 
-} // namespace wf
+void Window::createDefaultStyle() {
+    // ==============================================================
+    // Label
+    Ref<Style> labelStyle = new Style();
+	labelStyle->setColor(osg::Vec4(0.0, 0.0, 0.0, 1.0));
+	labelStyle->setFontFamily(std::string("prima_sans_bt.ttf"));
+	labelStyle->setFontSize(15);
+    addNamedStyle("Label", labelStyle.get());
 
+    // ==============================================================
+    // Listbox
+    Ref<Style> listboxStyle = new Style();
+	listboxStyle->setBorderWidth(1.0);
+	listboxStyle->setBorderColor(osg::Vec4(0.8, 0.8, 0.8, 1.0));
+    addNamedStyle("ListBox", listboxStyle.get());
+
+    Ref<Style> listboxItemStyle = new Style();
+	listboxItemStyle->setBorderWidth(1.0);
+	listboxItemStyle->setBorderColor(osg::Vec4(0.8, 0.8, 0.8, 1.0));
+    listboxItemStyle->setHeight(Style::UnitValue(Style::Pixels, 30));
+    addNamedStyle("ListBoxItem", listboxItemStyle.get());
+
+    Ref<Style> listboxItemSelectedStyle = new Style();
+	listboxItemSelectedStyle->setBorderWidth(1.0);
+	listboxItemSelectedStyle->setBorderColor(osg::Vec4(0.8, 0.8, 0.8, 1.0));
+	listboxItemSelectedStyle->setBackgroundColor(osg::Vec4(0.9, 0.9, 0.9, 1.0));
+    listboxItemSelectedStyle->setHeight(Style::UnitValue(Style::Pixels, 30));
+    addNamedStyle("ListBoxItem:selected", listboxItemSelectedStyle.get());
+
+    // ==============================================================
+    // Scrollbars
+	Ref<Style> verticalStyle = new Style();
+	verticalStyle->setWidth(Style::UnitValue(Style::Pixels, 22));
+	verticalStyle->setBackgroundColor(osg::Vec4(1.0, 1.0, 1.0, 1.0));
+	verticalStyle->setBorderWidth(1.0);
+	verticalStyle->setBorderColor(osg::Vec4(0.8, 0.8, 0.8, 1.0));
+	addNamedStyle("VerticalScrollBar", verticalStyle.get());
+
+	Ref<Style> horizontalStyle = new Style();
+	horizontalStyle->setHeight(Style::UnitValue(Style::Pixels, 22));
+	horizontalStyle->setBackgroundColor(osg::Vec4(1.0, 1.0, 1.0, 1.0));
+	horizontalStyle->setBorderWidth(1.0);
+	horizontalStyle->setBorderColor(osg::Vec4(0.8, 0.8, 0.8, 1.0));
+	addNamedStyle("HorizontalScrollBar", horizontalStyle.get());
+
+	Ref<Style> scrollLeftStyle = new Style();
+	scrollLeftStyle->setHeight(Style::UnitValue(Style::Pixels, 16));
+	scrollLeftStyle->setWidth(Style::UnitValue(Style::Pixels, 16));
+	scrollLeftStyle->setHorizontalAlign(std::string("left"));
+	scrollLeftStyle->setVerticalAlign(std::string("middle"));
+	scrollLeftStyle->setBackgroundColor(osg::Vec4(1.0, 1.0, 1.0, 1.0));
+	scrollLeftStyle->setBackgroundImage(std::string("go-left.png"));
+	addNamedStyle("ScrollLeftButton", scrollLeftStyle.get());
+
+	Ref<Style> scrollRightStyle = new Style();
+	scrollRightStyle->setHeight(Style::UnitValue(Style::Pixels, 16));
+	scrollRightStyle->setWidth(Style::UnitValue(Style::Pixels, 16));
+	scrollRightStyle->setHorizontalAlign(std::string("right"));
+	scrollRightStyle->setVerticalAlign(std::string("middle"));
+	scrollRightStyle->setTop(Style::UnitValue(Style::Pixels, 0));
+	scrollRightStyle->setBackgroundColor(osg::Vec4(1.0, 1.0, 1.0, 1.0));
+	scrollRightStyle->setBackgroundImage(std::string("go-right.png"));
+	addNamedStyle("ScrollRightButton", scrollRightStyle.get());
+
+	Ref<Style> scrollUpStyle = new Style();
+	scrollUpStyle->setHeight(Style::UnitValue(Style::Pixels, 16));
+	scrollUpStyle->setWidth(Style::UnitValue(Style::Pixels, 16));
+	scrollUpStyle->setHorizontalAlign(std::string("center"));
+	scrollUpStyle->setVerticalAlign(std::string("top"));
+	scrollUpStyle->setBackgroundColor(osg::Vec4(1.0, 1.0, 1.0, 1.0));
+	scrollUpStyle->setBackgroundImage(std::string("go-up.png"));
+	addNamedStyle("ScrollUpButton", scrollUpStyle.get());
+
+	Ref<Style> scrollDownStyle = new Style();
+	scrollDownStyle->setHeight(Style::UnitValue(Style::Pixels, 16));
+	scrollDownStyle->setWidth(Style::UnitValue(Style::Pixels, 16));
+	scrollDownStyle->setHorizontalAlign(std::string("center"));
+	scrollDownStyle->setVerticalAlign(std::string("bottom"));
+	scrollDownStyle->setBackgroundColor(osg::Vec4(1.0, 1.0, 1.0, 1.0));
+	scrollDownStyle->setBackgroundImage(std::string("go-down.png"));
+	addNamedStyle("ScrollDownButton", scrollDownStyle.get());
+
+    // ==============================================================
+    // Window
+    Ref<Style> windowStyle = new Style();
+	windowStyle->setBackgroundColor(osg::Vec4(1, 1, 1, 10));
+	windowStyle->setBorderWidth(1.0);
+	windowStyle->setBorderColor(osg::Vec4(0, 0, 0, 1));
+	addNamedStyle("Window", windowStyle.get());
+}
+
+} // namespace wf
 } // namespace csp
--- csp/cspwf/Window.h
+++ csp/cspwf/Window.h
@@ -47,9 +47,9 @@ typedef std::map<std::string, Ref<Style> > NamedStyleMap;
 
 /** A Control that can be displayed using the WindowManager class. This class is the
  * top level Control that holds all other child controls. This Control has no parent.
- * 
+ *
  * By default this class only supports a single child control that will take up all
- * of the client area. 
+ * of the client area.
  */
 class CSPWF_EXPORT Window : public SingleControlContainer {
 public:
@@ -58,63 +58,69 @@ public:
 
 	/** Sets the WindowManager that this Window belongs to. */
 	virtual void setWindowManager(WindowManager* manager);
-	
+
 	/** Retreives the WindowManager that this Window belongs to. */
 	virtual WindowManager* getWindowManager();
-	
+
 	virtual void performLayout();
 	virtual void layoutChildControls();
-	
+
 	/** Closes this window. All controls are removed and the window cannot
 	 * be reused or displayed again.
 	 */
 	virtual void close();
-	
-	/** Centers the window in the middle of the screen. 
+
+	/** Centers the window in the middle of the screen.
 	 * Note that this setting also can be set using the Style object.
 	 */
 	virtual void centerWindow();
-	
-	/** Maximizes the window to take up the entire screen. 
+
+	/** Maximizes the window to take up the entire screen.
 	 * Note that this setting also can be set using the Style object.
 	 */
 	virtual void maximizeWindow();
 
-	/** Returns the Style with the name. A window holds a map with all 
+    /** Creates default styles for all window controls.
+     * Is very usefull when creating an example application to a
+     * very basic visual style.
+     */
+    virtual void createDefaultStyle();
+
+	/** Returns the Style with the name. A window holds a map with all
 	 * named style objects that has been declared within xml file for the
-	 * window. All included style files is also added to this map. These 
-	 * named style objects can then be used by any child control to this 
+	 * window. All included style files is also added to this map. These
+	 * named style objects can then be used by any child control to this
 	 * window.
-	 */	
+	 */
 	virtual optional<Ref<Style> > getNamedStyle(const std::string& name) const;
-	
+
 	/** Adds a named style to the styles map. This named style can then be
 	 * used by any child control using the CssClass.
 	 */
 	virtual void addNamedStyle(const std::string& name, Style* style);
 
-	/** Returns the Window that the Control belongs to. If the control hasn't 
+	/** Returns the Window that the Control belongs to. If the control hasn't
 	 * been assigned a window yet this method returns NULL.
 	 */
 	static Window* getWindow(Control* control);
 
-	/** Returns the Window that the Control belongs to. If the control hasn't 
+	/** Returns the Window that the Control belongs to. If the control hasn't
 	 * been assigned a window yet this method returns NULL.
 	 */
 	static const Window* getWindow(const Control* control);
 
 	virtual void setTheme(const std::string& theme);
 	virtual std::string getTheme() const;
-	
+
 	virtual StringResourceManager* getStringResourceManager();
 	virtual const StringResourceManager* getStringResourceManager() const;
-	
+
 	template<class Archive>
 	void serialize(Archive & ar)	{
 		SingleControlContainer::serialize(ar);
 		ar & make_nvp("Styles", m_Styles);
 	}
-	
+
 private:
 	WeakRef<WindowManager> m_WindowManager;
 	std::string m_Theme;

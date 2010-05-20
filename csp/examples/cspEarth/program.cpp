105a8b22dc54ef3713475e3ae97a5a89e66c8a3c
Martin Hoffmann <StormbrigerCSP@web.de>
2010-05-20 20:39:17 +0200

    added input handler

--- csp/examples/cspEarth/program.cpp
+++ csp/examples/cspEarth/program.cpp
@@ -22,13 +22,20 @@
 #include <osgViewer/Viewer>
 #include <osgViewer/ViewerEventHandlers>
 #include <osgViewer/GraphicsWindow>
+#include <osgViewer/ViewerBase>
 #include <osgDB/ReadFile>
+#include <osgDB/WriteFile>
+#include <osgGA/FlightManipulator>
 #include <osg/Group>
 
 #include <string>
 #include <iostream>
 
+#include "InputHandler.h"
+
 using namespace csp;
+
+osgViewer::Viewer viewer;
 
 /** This sample application will manually create a single object model
 *  to be displayed in a scene. A object model represents basic info
@@ -38,9 +45,91 @@ using namespace csp;
 *  are normally serialized directly from xml files.
 */
 
+void setupCameraAndContext(osgViewer::Viewer& viewer, int windowWidth, int windowHeight)
+{
+    osg::GraphicsContext::WindowingSystemInterface* wsi = osg::GraphicsContext::getWindowingSystemInterface();
+    if (!wsi) 
+    {
+        osg::notify(osg::NOTICE)<<"Error, no WindowSystemInterface available, cannot create windows."<<std::endl;
+        return;
+    }
+    
+    unsigned int width, height;
+    wsi->getScreenResolution(osg::GraphicsContext::ScreenIdentifier(0), width, height);
+
+	osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
+	traits->x = 0;
+	traits->y = 0;
+	// use either system native screen size or what was specified in command line
+	if (windowWidth > 0 && windowHeight > 0)
+	{
+		traits->width = windowWidth;
+		traits->height = windowHeight;
+		traits->x = 50;
+		traits->y = 50;
+	}
+	else
+	{
+		traits->width = width;
+		traits->height = height;
+	}
+    traits->windowDecoration = true;
+    traits->doubleBuffer = true;
+    traits->sharedContext = 0;
+
+    osg::ref_ptr<osg::GraphicsContext> gc = osg::GraphicsContext::createGraphicsContext(traits.get());
+    if (gc.valid())
+    {
+        osg::notify(osg::INFO)<<"  GraphicsWindow has been created successfully."<<std::endl;
+
+        // need to ensure that the window is cleared make sure that the complete window is set the correct colour
+        // rather than just the parts of the window that are under the camera's viewports
+        gc->setClearColor(osg::Vec4f(0.2f,0.2f,0.6f,1.0f));
+        gc->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+    else
+    {
+        osg::notify(osg::NOTICE)<<"  GraphicsWindow has not been created successfully."<<std::endl;
+    }
+
+    unsigned int numCameras = 1;
+    double aspectRatioScale = 1.0;///(double)numCameras;
+    osg::ref_ptr<osg::Camera> camera = new osg::Camera;
+    camera->setGraphicsContext(gc.get());
+	if (windowWidth > 0 && windowHeight > 0)
+		camera->setViewport(new osg::Viewport(0, 0, windowWidth, windowHeight));
+	else
+		camera->setViewport(new osg::Viewport(0, 0, width, height));
+    GLenum buffer = traits->doubleBuffer ? GL_BACK : GL_FRONT;
+    camera->setDrawBuffer(buffer);
+    camera->setReadBuffer(buffer);
+
+    viewer.addSlave(camera.get(), osg::Matrixd(), osg::Matrixd::scale(aspectRatioScale,1.0,1.0));
+}
+
+void dumpSceneToDisk()
+{
+	std::string filename = "dump";
+	osgViewer::ViewerBase::Scenes viewerScenes;
+	osgViewer::Scene* scene;
+
+	viewer.getScenes(viewerScenes, true);
+	for (osgViewer::ViewerBase::Scenes::iterator it = viewerScenes.begin(); it!=viewerScenes.end(); ++it)
+	{
+		scene = *it;
+		osg::Node* node = scene->getSceneData();
+
+		bool result = osgDB::writeNodeFile(*node, filename + ".osg" );
+
+		if ( !result )
+			osg::notify(osg::FATAL) << "Failed in osgDB::writeNodeFile()." << std::endl;
+	}
+}
+
 int main( int argc, char **argv )
 {
-	std::string osgEarthFileName = "D:/Dev/OSG/osgEarth/tests/srtm.earth";
+	int windowWidth, windowHeight;
+	std::string osgEarthFileName = "D:/Dev/csp/csp/examples/cspEarth/data/srtm.earth";
 
 	// use an ArgumentParser object to manage the program arguments.
     osg::ArgumentParser arguments(&argc,argv);
@@ -48,12 +137,15 @@ int main( int argc, char **argv )
 	// Allow a debugger to be attached before the interesting parts start
 	if (arguments.read("-p"))
 	{
-		std::cout << "Attach debugger and press a key...\n";
+		std::cout << "Attach debugger and press a key..." << std::endl;
 		std::getchar();
 	}
 
 	// if an .earth file definition was specified, use this one instead of the default
 	arguments.read("-file", osgEarthFileName);
+
+	// get screen size
+	arguments.read("-window", windowWidth, windowHeight);
 
 	// =====================================================================
 	// SETUP
@@ -84,28 +176,41 @@ int main( int argc, char **argv )
 	// smoothing and more things.
 	my_model->loadModel();
 
-	std::cout << "reading osgEarth config file: " << osgEarthFileName << "\n";
-	osg::ref_ptr<osg::Node> globe = osgDB::readNodeFile("D:/Dev/OSG/osgEarth/tests/srtm.earth");
+	std::cout << "reading osgEarth config file: " << osgEarthFileName << std::endl;
+	osg::ref_ptr<osg::Node> globe = osgDB::readNodeFile(osgEarthFileName);
 	osg::ref_ptr<osg::Node> topNode = new osg::Node;
 
 	osg::Group* group = new osg::Group;
 
 	// =====================================================================
 	// construct the viewer
-	osgViewer::Viewer viewer;
 	viewer.addEventHandler(new osgViewer::StatsHandler());
 	viewer.addEventHandler(new osgViewer::HelpHandler());
 
 	// Set scene data
-	group->addChild(globe);
+	group->addChild(globe.get());
 	group->addChild(my_model->getModel().get());
 
-	// viewer.setSceneData(my_model->getModel().get());
+	//viewer.setSceneData(my_model->getModel().get());
 	viewer.setSceneData(group);
+	//viewer.setSceneData(globe.get());
 
-	osgViewer::GraphicsWindowEmbedded* gfxWindow;
-//	gfxWindow = viewer.setUpViewerAsEmbeddedInWindow(100, 100, 1000, 800);
+	// create camera and context
+	setupCameraAndContext(viewer, windowWidth, windowHeight);
 
+	//dumpSceneToDisk(group, "test");
+	//dumpSceneToDisk(globe.get(), "globe");
+	//dumpSceneToDisk(my_model->getModel().get(), "object");
+
+	viewer.setCameraManipulator(new osgGA::FlightManipulator);
+
+	viewer.addEventHandler(new inputHandler());
 	// run the viewers frame loop
-	return viewer.run();
+    viewer.realize();
+
+    // main loop (note, window toolkits which take control over the main loop will require a window redraw callback containing the code below.)
+    while(!viewer.done())
+    {
+        viewer.frame();
+    }
 }

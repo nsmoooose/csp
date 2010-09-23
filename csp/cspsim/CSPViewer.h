7945bd8507a266bcd06f52bfeebff7221639c699
Peter C <pciii@pciii-laptop.(none)>
2010-09-23 15:06:07 -0400

    Converted comments to proper Doxygen comments.

--- csp/cspsim/CSPViewer.h
+++ csp/cspsim/CSPViewer.h
@@ -16,16 +16,18 @@ namespace csp {
 		class WindowManagerViewer;
 	}
 
-	/* Base class that encapsulates the osgViewer::Viewer class. This
-	   class will run the main loop and is responsible for holding the
-	   entire scene and render it. 
-
-	   As default it will create a osg::Group and add set it on the viewer.
-	   So it is only necessarry to add new children to it in order to display 
-	   data.
-
-	   It is also exported to python meaning it can be used from python
-	   to start the simulator or do other things.*/
+	/**
+	 * Base class that encapsulates the osgViewer::Viewer class. This
+	 * class will run the main loop and is responsible for holding the
+	 * entire scene and render it. 
+	 * 
+	 * As default it will create a osg::Group and add set it on the viewer.
+	 * So it is only necessarry to add new children to it in order to display 
+	 * data.
+	 * 
+	 * It is also exported to python meaning it can be used from python
+	 * to start the simulator or do other things.
+	 */
 	class CSPSIM_EXPORT CSPViewer {
 	public:
 		CSPViewer();
@@ -36,9 +38,9 @@ namespace csp {
 
 		virtual void addChild(osg::Node* node);
 
-		/* Provides statistics information if you press the s key. */
+		/** Provides statistics information if you press the s key. */
 		virtual void addStatsHandler();
-		/* Provides help if you press the h key. */
+		/** Provides help if you press the h key. */
 		virtual void addHelpHandler();
 
 		virtual void addWindowManagerEventHandler(wf::WindowManagerViewer* windowManager);

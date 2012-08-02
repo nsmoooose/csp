c3b2476acab4c00215e2fd22a782b5f36389e956
Henrik Nilsson <henrik.nilsson@tvaaker.se>
2012-08-02 22:21:38 +0200

    Remove binary files and support for cmake.
    The CSP devpack doesn't belong in version control. So I simply removed
    all traces of it.
    
    The support for cmake wasn't completely finished (didn't work for me).
    So I removed it as well.

--- CMakeModules/FindCSPDEVPACK.cmake
+++
@@ -1,9 +0,0 @@
-FIND_PATH( DEVPACK_INCLUDE_DIR jpeglib.h
-	$ENV{CSP_DEVPACK}/include
-    ${CMAKE_SOURCE_DIR}/cspdevpack-0.7/include
-)
-
-FIND_PATH( DEVPACK_LIB_DIR jpeg.lib
-	$ENV{CSP_DEVPACK}/lib
-    ${CMAKE_SOURCE_DIR}/cspdevpack-0.7/lib
-)

bc666bcbd6c27db6194517454b14cf791726bf86
Henrik Nilsson <nsmoooose@gmail.com>
2019-01-27 16:24:27 +0100

    some imp

--- csp/tools/layout2/layout.py
+++ csp/tools/layout2/layout.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import os
 import csp.csplib
 import csp.cspsim
@@ -7,32 +6,35 @@ import csp.cspsim
 from csp.tools.layout2 import layout_module
 from csp.tools.layout2.scripts.ui.LayoutApplication import LayoutApplication
 
+
 def loadModules():
-	modules = ('chunklod', 'demeter')
-	extension = {
-		'posix' : '.so',
-		'nt' : '.dll',
-	}.get(os.name, '')
+    modules = ('chunklod', 'demeter')
+    extension = {
+        'posix': '.so',
+        'nt': '.dll',
+    }.get(os.name, '')
 
-	prefix = {
-		'posix' : 'lib',
-		'nt' : '',
-	}.get(os.name, '')
+    prefix = {
+        'posix': 'lib',
+        'nt': '',
+    }.get(os.name, '')
 
-	for module in modules:
-		module_path = os.path.join('..', '..', 'modules', module, '.bin', prefix + module) + extension
-		# for windows demos, the modules are instead placed in the current directory.
-		# TODO move them to ../modules/*.dll?
-		if not os.path.exists(module_path):
-			module_path = os.path.join(module) + extension
-		if not csp.csplib.ModuleLoader.load(module_path):
-			print(('Unable to load required extension module "%s"' % module))
+    for module in modules:
+        module_path = os.path.join('..', '..', 'modules', module, '.bin', prefix + module) + extension
+        # for windows demos, the modules are instead placed in the current directory.
+        # TODO move them to ../modules/*.dll?
+        if not os.path.exists(module_path):
+            module_path = os.path.join(module) + extension
+        if not csp.csplib.ModuleLoader.load(module_path):
+            print(('Unable to load required extension module "%s"' % module))
 
-loadModules()
 
-# Make all objects available for introspection and serialization
-csp.cspsim.registerAllObjectInterfaces()
+if __name__ == "__main__":
+    loadModules()
 
-# Create the application object and run the main loop.
-app = LayoutApplication(0)
-app.MainLoop()
+    # Make all objects available for introspection and serialization
+    csp.cspsim.registerAllObjectInterfaces()
+
+    # Create the application object and run the main loop.
+    app = LayoutApplication(0)
+    app.MainLoop()

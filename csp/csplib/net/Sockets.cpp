e1c7f947e5c6f7576d002b08e4dbdc206250a94f
Henrik Nilsson <nsmoooose@gmail.com>
2025-03-10 09:45:53 +0100

    Remove inline calls for sockets
    A lot of premature optimizations where code shouldn't be
    inlined.

--- csp/csplib/net/Sockets.cpp
+++ csp/csplib/net/Sockets.cpp
@@ -40,8 +40,102 @@ static bool waitFor(
 	return true;
 }
 
+DatagramReceiveSocket::DatagramReceiveSocket(boost::asio::io_context &context, const boost::asio::ip::address &host, unsigned short port) :
+	m_socket(context), m_endpoint(host, port) {
+	m_socket.open(boost::asio::ip::udp::v4());
+	m_socket.bind(m_endpoint);
+}
+
+void DatagramReceiveSocket::connect(const boost::asio::ip::address &host, unsigned short port) {
+	m_endpoint = boost::asio::ip::udp::endpoint(host, port);
+	m_socket.open(boost::asio::ip::udp::v4());
+	m_socket.bind(m_endpoint);
+}
+
+void DatagramReceiveSocket::disconnect(void) {
+	m_socket.close();
+}
+
+boost::asio::ip::address DatagramReceiveSocket::getSender(unsigned short *port) const {
+	if(port) {
+		*port = m_sender_endpoint.port();
+	}
+	return m_sender_endpoint.address();
+}
+
+int DatagramReceiveSocket::receive(void *buffer, std::size_t len) {
+	return m_socket.receive_from(boost::asio::buffer(buffer, len), m_sender_endpoint);
+}
+
+int DatagramReceiveSocket::receive(void* buffer, std::size_t len, int timeoutMilliseconds) {
+	auto timeout = std::chrono::milliseconds(timeoutMilliseconds);
+	auto &context = getIOContext();
+	boost::asio::steady_timer timer(context);
+	boost::system::error_code ec;
+	std::size_t received_bytes = 0;
+	bool receive_completed = false;
+
+	// Start the asynchronous receive
+	m_socket.async_receive_from(
+		boost::asio::buffer(buffer, len),
+		m_sender_endpoint,
+		[&](const boost::system::error_code& error, std::size_t bytes_transferred) {
+			ec = error;
+			received_bytes = bytes_transferred;
+			receive_completed = true;
+			// Cancel the timeout timer
+			timer.cancel();
+		});
+
+	// Set up a timer for the timeout duration
+	timer.expires_after(timeout);
+	timer.async_wait([&](const boost::system::error_code& error) {
+		if (!error && !receive_completed) {
+			// Cancel the socket operation if it’s still pending
+			m_socket.cancel();
+		}
+	});
+
+	// Run the I/O context to process the operations
+	context.run();
+	// Reset the context for future calls
+	context.restart();
+
+	if (ec == boost::asio::error::operation_aborted) {
+		// Indicate timeout
+		return -1;
+	} else if (ec) {
+		// Throw an error if any occurred
+		throw boost::system::system_error(ec);
+	}
+
+	return static_cast<int>(received_bytes);
+}
+
+int DatagramReceiveSocket::peek(void *buf, size_t len) {
+	return m_socket.receive_from(
+		boost::asio::buffer(buf, len),
+		m_sender_endpoint, boost::asio::socket_base::message_peek);
+}
+
 bool DatagramReceiveSocket::isPendingReceive(timeout_t timeout) {
 	return waitFor(m_socket, boost::asio::socket_base::wait_read, timeout);
+}
+
+DatagramTransmitSocket::DatagramTransmitSocket(boost::asio::io_context &context) : m_socket(context) {
+}
+
+void DatagramTransmitSocket::connect(const boost::asio::ip::address &host, unsigned short port) {
+	m_endpoint = boost::asio::ip::udp::endpoint(host, port);
+	m_socket.open(boost::asio::ip::udp::v4());
+}
+
+void DatagramTransmitSocket::disconnect(void) {
+	m_socket.close();
+}
+
+int DatagramTransmitSocket::transmit(const char* data, std::size_t size) {
+	return m_socket.send_to(boost::asio::buffer(data, size), m_endpoint);
 }
 
 bool DatagramTransmitSocket::isOutputReady(timeout_t timeout) {
--- csp/csplib/net/Sockets.h
+++ csp/csplib/net/Sockets.h
@@ -43,85 +43,22 @@ CSPLIB_EXPORT boost::asio::io_context &getIOContext();
  */
 class CSPLIB_EXPORT DatagramReceiveSocket {
 public:
-	DatagramReceiveSocket(boost::asio::io_context &context, const boost::asio::ip::address &host, unsigned short port) :
-		m_socket(context), m_endpoint(host, port) {
-		m_socket.open(boost::asio::ip::udp::v4());
-		m_socket.bind(m_endpoint);
-	}
+	DatagramReceiveSocket(boost::asio::io_context &context, const boost::asio::ip::address &host, unsigned short port);
 
-	void connect(const boost::asio::ip::address &host, unsigned short port) {
-		m_endpoint = boost::asio::ip::udp::endpoint(host, port);
-		m_socket.open(boost::asio::ip::udp::v4());
-		m_socket.bind(m_endpoint);
-	}
+	void connect(const boost::asio::ip::address &host, unsigned short port);
 
-	void disconnect(void) {
-		m_socket.close();
-	}
+	void disconnect(void);
 
 	bool isPendingReceive(timeout_t timeout);
 
-	boost::asio::ip::address getSender(unsigned short *port = NULL) const {
-		if(port) {
-			*port = m_sender_endpoint.port();
-		}
-		return m_sender_endpoint.address();
-	}
+	boost::asio::ip::address getSender(unsigned short *port = NULL) const;
 
-	inline int receive(void *buffer, std::size_t len) {
-		return m_socket.receive_from(boost::asio::buffer(buffer, len), m_sender_endpoint);
-	}
+	int receive(void *buffer, std::size_t len);
 
-	template <typename Rep, typename Period>
-	int receive(void* buffer, std::size_t len, const std::chrono::duration<Rep, Period>& timeout) {
-		auto &context = getIOContext();
-		boost::asio::steady_timer timer(context);
-		boost::system::error_code ec;
-		std::size_t received_bytes = 0;
-		bool receive_completed = false;
+	int receive(void* buffer, std::size_t len, int timeoutMilliseconds);
 
-		// Start the asynchronous receive
-		m_socket.async_receive_from(
-			boost::asio::buffer(buffer, len),
-			m_sender_endpoint,
-			[&](const boost::system::error_code& error, std::size_t bytes_transferred) {
-				ec = error;
-				received_bytes = bytes_transferred;
-				receive_completed = true;
-				// Cancel the timeout timer
-				timer.cancel();
-			});
+	int peek(void *buf, size_t len);
 
-		// Set up a timer for the timeout duration
-		timer.expires_after(timeout);
-		timer.async_wait([&](const boost::system::error_code& error) {
-			if (!error && !receive_completed) {
-				// Cancel the socket operation if it’s still pending
-				m_socket.cancel();
-			}
-		});
-
-		// Run the I/O context to process the operations
-		context.run();
-		// Reset the context for future calls
-		context.restart();
-
-		if (ec == boost::asio::error::operation_aborted) {
-			// Indicate timeout
-			return -1;
-		} else if (ec) {
-			// Throw an error if any occurred
-			throw boost::system::system_error(ec);
-		}
-
-		return static_cast<int>(received_bytes);
-	}
-
-	inline int peek(void *buf, size_t len) {
-		return m_socket.receive_from(
-			boost::asio::buffer(buf, len),
-			m_sender_endpoint, boost::asio::socket_base::message_peek);
-	}
 private:
 	boost::asio::ip::udp::socket m_socket;
 	boost::asio::ip::udp::endpoint m_endpoint;
@@ -135,21 +72,13 @@ private:
  */
 class CSPLIB_EXPORT DatagramTransmitSocket {
 public:
-	DatagramTransmitSocket(boost::asio::io_context &context) : m_socket(context) {
-	}
+	DatagramTransmitSocket(boost::asio::io_context &context);
 
-	inline void connect(const boost::asio::ip::address &host, unsigned short port) {
-		m_endpoint = boost::asio::ip::udp::endpoint(host, port);
-		m_socket.open(boost::asio::ip::udp::v4());
-	}
+	void connect(const boost::asio::ip::address &host, unsigned short port);
 
-	inline void disconnect(void) {
-		m_socket.close();
-	}
+	void disconnect(void);
 
-	inline int transmit(const char* data, std::size_t size) {
-		return m_socket.send_to(boost::asio::buffer(data, size), m_endpoint);
-	}
+	int transmit(const char* data, std::size_t size);
 
 	bool isOutputReady(timeout_t timeout=0);
 
